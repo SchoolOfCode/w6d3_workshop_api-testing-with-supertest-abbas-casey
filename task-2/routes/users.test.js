2a807147f088d4144dbaf9475776dd46dc269917
EzyhB <ezyhbmail@gmail.com>
2021-12-22 12:05:18 +0000

    task 2 done

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -1,1 +1,26 @@
 // Write your tests for task 2 in this file. Plan out what you need to import/require.
+import request from "supertest";
+import app from "../app.js";
+
+describe("async function", () => {
+    it("should send req to /Users", async() => {
+        await request(app)
+            .get("/users")
+            .expect(200)
+            .expect((res) => {
+                const expected = { success: true, payload: expect.any(Array) };
+
+                const actual = res.body;
+                expect(actual).toStrictEqual(expected);
+            })
+            .expect((res) => {
+                const actual = res.body.payload;
+                const expected = expect.arrayContaining([{
+                    id: expect.any(Number),
+                    username: expect.any(String),
+                }, ]);
+
+                expect(actual).toStrictEqual(expected);
+            });
+    });
+});
