5b4c72d3b2c5b3ae0569c9b61506f494c737bdda
EzyhB <ezyhbmail@gmail.com>
2021-12-22 13:57:56 +0000

    fast

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -26,15 +26,34 @@ describe("async function", () => {
 });
 
 describe("GET /users/4", () => {
-    it("should give a response with the correct payload structure", async () => {
+    it("should give a response with the correct payload structure", async() => {
         await request(app)
             .get("/users/4")
             .expect(200)
-            .expect( (res) => {
-                const expected = { success: true, 
-                                   payload: { id: 4, username: expect.any(String) } };
+            .expect((res) => {
+                const expected = {
+                    success: true,
+                    payload: { id: 4, username: expect.any(String) },
+                };
                 const actual = res.body;
                 expect(actual).toStrictEqual(expected);
-            })
+            });
+    });
+});
+
+describe("getting error code 404?", () => {
+    it("should pass with error 404", async() => {
+        await request(app)
+            .get("/users/99")
+            .expect(404)
+            .expect((res) => {
+                const actual = res.body;
+                const expected = {
+                    success: false,
+                    reason: "No user with ID 99 was found.",
+                };
+
+                expect(actual).toStrictEqual(expected);
+            });
     });
 });
